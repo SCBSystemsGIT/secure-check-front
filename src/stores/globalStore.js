2ca7a581ca2f67c874fd6e7d70df14a696b6f789
sidikfofana <kdyd2005@gmail.com>
2024-10-29 15:12:27 +0000

    Last update line

--- src/stores/globalStore.js
+++ src/stores/globalStore.js
@@ -3,9 +3,9 @@ import { defineStore } from "pinia";
 export const useGlobalStore = defineStore("global", {
   
   state: () => ({
-    baseUrl: "http://52.45.12.88/api/public/index.php/api",
-    baseUrl_: "http://52.45.12.88/api/public/index.php",
-    publicDir: "http://52.45.12.88/api/public",
+    baseUrl: "https://www.securecheck.info/api/public/index.php/api",
+    baseUrl_: "https://www.securecheck.info/api/public/index.php",
+    publicDir: "https://www.securecheck.info/api/public",
     
     // baseUrl: "http://localhost:9999/api",
     // baseUrl_: "http://localhost:9999",
